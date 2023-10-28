92045a1956e78f9cb22a710d08922b2c318a95fa
ddenzu <112197281+ddenzu@users.noreply.github.com>
2023-10-29 02:33:17 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,17 +1,9 @@
-T = int(input())
+for _ in range(int(input())):
+    a = int(input())
+    b = int(input())
+    A = [ i for i in range(1, b+1)]
 
-for _ in range(T):
-
-    k = int(input())
-
-    n = int(input())
-
-    people = [ i for i in range(1, n+1)]
-
-    for __ in range(k):
-
-        for j in range(1,n):
-
-            people[j] += people[j-1]
-
-    print(people[-1])
+    for i in range(a):
+        for j in range(1,b):
+            A[j]+= A[j-1]
+    print(A[-1])
